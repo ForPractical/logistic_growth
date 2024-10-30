6cbb88be1bb60fdb5ed15d96e361432aac7daa19
ForPractical <hanzhang.wu@chch.ox.ac.uk>
2024-10-30 15:50:47 +0000

    commit

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,18 +10,31 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
-  
-r <- ??? #
-  
-K <- ??? #
+exponential_fun <- function(t){
+  N<-N0*exp(r*t)
+  return(N)
+}
 
-ggplot(aes(???,???), data = growth_data) +
+N0 <- 879
+
+r <- 0.01
+
+K <- 60000000000
+
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
-  
-  geom_point()
+  geom_function(fun=exponential_fun,color="blue")+
+  scale_y_continuous(trans='log10')+
+  geom_point(size=0.6)+
+  labs(title="Exponential and logistic growth of population",
+       y="log(N), N=population size",
+       x="time t (min)")
 
-  #scale_y_continuous(trans='log10')
 
+#scale_y_continuous(trans='log10')
+#note the position between t and N
 
+sink(file = "package-versions.txt")
+sessionInfo()
+sink()
